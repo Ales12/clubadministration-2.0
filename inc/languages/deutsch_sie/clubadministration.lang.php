ab2a14dd0bc169f5922a7dd0a2b008c49a9c08ea
Alexandra <38242434+Ales12@users.noreply.github.com>
2020-12-07 13:04:35 +0100

    Update clubadministration.lang.php

--- inc/languages/deutsch_sie/clubadministration.lang.php
+++ inc/languages/deutsch_sie/clubadministration.lang.php
@@ -1,13 +1,14 @@
 <?php
 $l['clubs'] = "Clubübersicht";
 $l['club_welcome'] = "Willkommen bei der Club Übersicht";
+$l['club_view'] = "Clubübersicht";
 $l['club_add'] = "Einen Club hinzufügen";
 $l['club_name'] = "Clubname";
 $l['club_desc'] = "Clubbeschreibung";
 $l['club_cat'] = "Clubkategorie";
+$l['club_all'] = "alle Eingetragenen Clubs";
 $l['club_leader'] = "Clubführung";
 $l['club_members'] = "Mitglieder";
-$l['club_all'] = "alle Eingetragenen Clubs";
 $l['club_own'] = "Übersicht eigene Clubs";
 $l['club_edit'] = "Club editieren";
 $l['club_modcp'] = "Komplette Clubverwaltung";
@@ -16,7 +17,9 @@ $l['club_option'] = "Annehmen/Ablehnen";
 $l['club_ok'] = "Club wurde angenommen";
 $l['club_ok_text'] = "Dein Club wurde angenommen. Du kannst dich nun unter <a href='misc.php?action=clubs'>Clubübersicht</a> eintragen.";
 $l['club_no'] = "Club wurde abgelehnt";
-$l['club_no_text'] = "Dein Club wurde abgelehnt. Du kannst ihn unter <a href='misc.php?action=ownclubs'>Eigene Clubs</a> bearbeiten. <br />Du kannst beim Team gerne erfragen, wieso dein Club abgelehnt wurde.";
+$l['club_no_text'] = "Dein Club wurde abgelehnt. Sie können es unter <a href='misc.php?action=ownclubs'>Eigene Clubs</a> bearbeiten. <br />Sie können beim Team gerne erfragen, wieso dein Club abgelehnt wurde.";
+$l['club_all'] = "alle vorhandenen Clubs";
+$l['club_options'] = "Editieren/Löschen";
 $l['club_creator'] = "Clubersteller";
 $l['club_memprofile'] = "Eingeschriebene Clubs";
 $l['club_leader'] = "Clubführung";
