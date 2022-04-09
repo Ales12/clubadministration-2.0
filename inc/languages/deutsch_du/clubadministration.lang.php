a268416c9a2da76fca8ba4fe70542998258d939f
Alexandra <38242434+Ales12@users.noreply.github.com>
2022-04-09 13:58:18 +0200

    Update clubadministration.lang.php

--- inc/languages/deutsch_du/clubadministration.lang.php
+++ inc/languages/deutsch_du/clubadministration.lang.php
@@ -1,20 +1,27 @@
 <?php
-$l['clubandsociety_title'] = "Club- und Vereinsverwaltung";
+$l['clubandsociety_title'] = "Club- und Vereinverwaltung";
 
 // Formular
 $l['clubandsociety_add'] = "Einen neuen club oder Verein hinzufügen";
 $l['clubandsociety_addtitle'] = "Name des Clubs/Vereins:";
 $l['clubandsociety_addtype'] = "Club oder Verein?";
 $l['clubandsociety_addcat'] = "Kategorie:";
-$l['clubandsociety_addadmin'] = "Bist du auch Clubführer?";
+$l['clubandsociety_addadmin'] = "Bist du auch die Leitung?";
 $l['clubandsociety_adddesc'] = "Beschreibe hier kurz den Club:";
 $l['clubandsociety_addsubmit'] = "Club/Verein hinzufügen";
 
+
 // Clubübersicht
-$l['clubandsociety_overview_leader'] = "Geführt von ";
+$l['clubandsociety_overview_leader'] = "Geleitet von ";
 $l['clubandsociety_overview_member'] = "Mitglieder";
 $l['clubandsociety_overview_join'] = "Club/Verein eintreten";
 $l['clubandsociety_overview_leave'] = "verlassen";
+$l['clubandsociety_join_leader'] = "Leitung übernehmen";
+$l['clubandsociety_join_leader_down'] = "Leitung abgeben";
+
+$l['clubandsociety_filter_type'] = "Nach Art filtern";
+$l['clubandsociety_filter_cat'] = "Nach Kategorie filtern";
+$l['clubandsociety_filter_submit'] = "Ansicht filtern";
 
 // Modcp
 $l['clubandsociety_modcp_nav'] = "Clubs/Vereine verwalten";
@@ -29,7 +36,7 @@ $l['clubandsociety_modcp_decline'] = "Ablehnen";
 $l['clubandsociety_delete'] = "Löschen";
 $l['clubandsociety_edit'] = "Bearbeiten";
 $l['clubandsociety_editsubmit'] = "Club/Verein bearbeiten";
-$l['clubandsociety_edit_leader'] = "Geführt von
+$l['clubandsociety_edit_leader'] = "Geleitet von
 <div class='smalltext'>Nur UID angeben!</div>";
 
 // user cp
@@ -39,3 +46,5 @@ $l['clubandsociety_ucp'] = "Übersicht eigener Clubs/Vereine";
 // Alerts
 $l['clubandsociety_accepted'] = "{1} hat deinen {2} <b>{3}</b> akzeptiert.";
 $l['clubandsociety_rejected'] = "{1} hat deinen {2} <b>{3}</b> abgelehnt. Melde dich beim Team, wenn du Fragen dazu hast!";
+$l['myalerts_settings_clubandsociety_accepted'] = "Möchtest du eine Benachrichtung bekommen, wenn dein Club angenommen wurde?";
+$l['myalerts_settings_clubandsociety_rejected'] = "Möchtest du eine Benachrichtung bekommen, wenn dein Club abgelehnt wurde?";
